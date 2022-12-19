01e956fa1d5d940e782adfdf6bb742c9215771f2
Ray Dedhia <22286026+ray-dedhia@users.noreply.github.com>
2022-12-19 02:33:58 -0500

    Update

--- run_gdb.sh
+++ run_gdb.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-# wait until done building
-pid="$(pgrep -f ^../run_base_refrate)"
-while [ "$pid" = "" ]
-do
-    pid="$(pgrep -f ^../run_base_refrate)"
-done
-echo "PID=$pid"
+# get main process id
+x="$(pgrep -f '^specperl -I/local/spec/bin/common')"
+mainpid="$(echo $x | awk '{print $1;}')"
 
-# start logging
-sudo gdb -batch-silent \
-    -p $pid \
-    -ex "set logging file /local/gdb-c$1.log" \
-    -ex "set logging on" \
-    -ex "dir /local/glibc-2.31/sysdeps" \
-    -ex "catch syscall exit_group" \
-    -ex "c" \
-    -ex "info proc mappings" \
-    -ex "info registers" \
-    -ex "shell cat /proc/$pid/maps > /local/proc-maps-c$1.log" \
-    -ex "c" \
-    -ex "quit"
+# while runcpu running
+while [ -d /proc/$mainpid ]
+do
+    # get child process id
+    pid="$(pgrep -f ^../run_base_refrate)"
+    while [ "$pid" = "" ]
+    do
+        pid="$(pgrep -f ^../run_base_refrate)"
+    done
+    echo "PID=$pid"
+
+    # start logging
+    sudo gdb -batch-silent \
+        -p $pid \
+        -ex "set logging file /local/state/gdb-c$1-p$pid.log" \
+        -ex "set logging on" \
+        -ex "dir /local/glibc-2.31/sysdeps" \
+        -ex "catch syscall exit_group" \
+        -ex "c" \
+        -ex "info proc mappings" \
+        -ex "info registers" \
+        -ex "shell cat /proc/$pid/maps > /local/state/proc-maps-c$1-p$pid.log" \
+        -ex "c" \
+        -ex "quit"
+done
