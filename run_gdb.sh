b4b7a0ad8364cbe00397ba796d30159f5531fe80
Ray Dedhia <22286026+ray-dedhia@users.noreply.github.com>
2022-12-19 15:26:22 -0500

    Update

--- run_gdb.sh
+++ run_gdb.sh
@@ -9,8 +9,6 @@ echo "pid=$3"
 
 sudo gdb -batch-silent \
     -p $pid \
-    -ex "set logging file /proj/final-project/state/$fn/gdb-c$core-p$pid.log" \
-    -ex "set logging on" \
     -ex "dir /proj/final-project/glibc-2.31/sysdeps" \
     -ex "catch syscall exit_group" \
     -ex "c" \
@@ -18,11 +16,12 @@ sudo gdb -batch-silent \
          | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' \
          | while read start stop; do \
             gdb --batch --pid $pid -ex \
-            "dump memory /proj/final-project/state/$fn/proc-maps-c$core-p$pid-r$start-$stop.log 0x$start 0x$stop"; done
+            \"dump memory /proj/final-project/state/$fn/proc-maps-c$core-p$pid-r$start-$stop.log 0x$start 0x$stop\"; done" \
     -ex "c" \
     -ex "quit"
 
 # extra logging
-# -ex "shell cat /proc/$pid/maps > /proj/final-project/state/$fn/proc-maps-c$core-p$pid.log" \
-# -ex "info proc mappings" \
-# -ex "info registers" \
+# set logging file /proj/final-project/state/$fn/gdb-c$core-p$pid.log
+# set logging on
+# info proc mappings
+# info registers
