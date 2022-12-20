acbf5f9e142ba6b4c988e3ae9d669ab420716bf3
Ray Dedhia <22286026+ray-dedhia@users.noreply.github.com>
2022-12-19 20:07:56 -0500

    Update

--- run_gdb.sh
+++ run_gdb.sh
@@ -13,10 +13,14 @@ sudo gdb -batch-silent \
     -ex "catch syscall exit_group" \
     -ex "c" \
     -ex "shell grep r[-w]-p /proc/$pid/maps \
-         | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' \
-         | while read start stop; do \
-            gdb --batch --pid $pid -ex \
-            \"dump memory /proj/final-project/state/$fn/proc-maps-c$core-p$pid-r$start-$stop.log 0x$start 0x$stop\"; done" \
+         | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' > /proj/final-project/state/$fn/tmpfile-c$core-p$pid" \
+    -ex 'python nl = str(len(open("/proj/final-project/state/'$fn'/tmpfile-c'$core'-p'$pid'").readlines())); gdb.execute("set $flen = \"" + nl + "\"")' \
+    -ex "set $ln=0" \
+    -ex "while ($line < $flen)" \
+        -ex 'python fline=open("/proj/final-project/state/'$fn'/tmpfile-c'$core'-p'$pid'").readline().rstrip().split(); gdb.execute("set '$start' = \"" + fline[0] + "\""); gdb.execute("set '$stop' = \"" + fline[1] + "\"")' \
+        -ex "dump memory /proj/final-project/state/$fn/proc-maps-c$core-p$pid-r$start-$stop.log 0x$start 0x$stop" \
+        -ex "set $ln=$ln+1" \
+    -ex "done" \
     -ex "c" \
     -ex "quit"
 
